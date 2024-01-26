3fc1d31db6ea602c8a120c91b05c22762e0fee64
Peter Cavar <peter_c96@hotmail.com>
2024-01-26 11:10:45 +0100

    removing duplicate script

--- connecting-fileshare-to-vm-drive.ps1
+++ connecting-fileshare-to-vm-drive.ps1
@@ -15,21 +15,3 @@ $map = @{
     Credential = $credential
 }
 New-PSDrive @map
-
-$key = @{
-    String = "<storage-account-key>" ## Insert SA - KEY
-}
-$acctKey = ConvertTo-SecureString @key -AsPlainText -Force
-
-$cred = @{
-    ArgumentList = "Azure\myprivatestoragepcpc", $acctKey ## Replace with your SA
-}
-$credential = New-Object System.Management.Automation.PSCredential @cred
-
-$map = @{
-    Name = "Z" ## Provide prefered Drive-letter
-    PSProvider = "FileSystem"
-    Root = "\\myprivatestoragepcpc.file.core.windows.net\myprivatesharepcpc" ## Replace with your SA URL
-    Credential = $credential
-}
-New-PSDrive @map
